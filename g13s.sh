f9030dc4d40aa77d0a87f00f4217e6f166fb0f54
am5z <119353097+am5z@users.noreply.github.com>
2024-08-19 11:33:26 +0100

    Update g13s.sh

--- g13s.sh
+++ g13s.sh
@@ -8,8 +8,8 @@ sudo mkdir -p /etc/g13
 sudo curl -o /etc/g13/g13.js https://raw.githubusercontent.com/QZkU2W3GwrbeR6KcS7fsmAnYFhqCgdjHyuMz5tN/G13/main/g13.js
 cd /etc/g13
 sudo npm init -y
-sudo npm install dockerode fs-extra axios path glob crypto pm2 toml -g
-sudo npm install dockerode fs-extra axios path glob crypto pm2 toml
+sudo npm install dockerode fs-extra axios path glob crypto pm2 toml adm-zip child_process -g
+sudo npm install dockerode fs-extra axios path glob crypto pm2 toml adm-zip child_process
 sudo pm2 start /etc/g13/g13.js --name "g13"
 sudo pm2 save
 sudo pm2 startup
